39d379dcd31020840932e9349849a3c44da1a78d
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-10-24 21:48:57 +0200

    debugging

--- models/state.py
+++ models/state.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-"""State Module for HBNB project"""
-import models
-from os import getenv
-from models.base_model import Base, BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_t
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Design plan for a review table within the hbnb database"""
-    __tablename__ = "states"
-    name = Column(String(128),
-                  nullable=False)
-    cities = relationship("City",
-                          backref="state",
-                          cascade="delete")
+    """ State class / table model"""
+    __tablename__ = 'states'
+    if storage_t == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Returns all related city objects as a list."""
-            return [city for city in list(models.storage.all(City).values())
-                    if city.state_id == self.id]
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
